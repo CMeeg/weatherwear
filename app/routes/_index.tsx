4b1327218e9ba98aa565925ff7ace7ca4c60ca4c
Chris Meagher <cpmeeg@gmail.com>
2023-12-16 16:56:42 +0000

    Add basic text suggestions

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+import OpenAI from "openai"
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +9,64 @@ export const meta: MetaFunction = () => {
   ]
 }
 
+export const loader = async () => {
+  const location = "witney,uk"
+  const lang = "en"
+
+  const weatherResponse = await fetch(
+    `https://wttr.in/${location}?lang=${lang}&format=j1`
+  )
+
+  const weather = await weatherResponse.json()
+
+  const temperatureUnit = "Celcius"
+  const subject = "male person"
+  const culture = "en-GB"
+
+  const openai = new OpenAI({
+    apiKey: import.meta.env.OPENAI_API_KEY
+  })
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `You are helpful fashion assistant giving advice to a ${subject} on what to wear based on their current local weather. You will provide advice in their preferred language culture, which is ${culture}.`
+      },
+      {
+        role: "user",
+        content: `Based the weather data below,
+give me suggestions on how warmly to dress,
+ie pants or shorts, a light jacket or a warm jacket,
+a scarf and gloves or not, if I should carry an umbrella, etc.
+In your response, use temperature data from the weather data below
+throughout the day to explain your recommendation.
+Be as concise as possible.
+Assume I'll wear the same thing the whole day.
+Do not use a bulleted list.
+Use 2-3 sentences.
+Only use ${temperatureUnit}.`.replaceAll("\n", " ")
+      },
+      {
+        role: "user",
+        content: JSON.stringify(weather)
+      }
+    ],
+    model: "gpt-4-1106-preview",
+    max_tokens: 150
+  })
+
+  const text = completion.choices[0].message.content
+
+  return { message: text }
+}
+
 export default function Index() {
+  const { message } = useLoaderData<typeof loader>()
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <p>{message ?? "No response."}</p>
     </div>
   )
 }
