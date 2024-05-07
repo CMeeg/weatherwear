23f07319a5877405602105ec8ba43806b9a2517a
Chris Meagher <cpmeeg@gmail.com>
2024-05-07 20:21:48 +0100

    Include default culture in react aria locales plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 import localesPlugin from "@react-aria/optimize-locales-plugin"
 import { flatRoutes } from "remix-flat-routes"
+import { defaultLocale } from "./app/lib/i18n"
 
 let base = process.env.CDN_URL ?? "/"
 if (!base.endsWith("/")) {
@@ -19,8 +20,11 @@ export default defineConfig({
         return flatRoutes("routes", defineRoutes)
       }
     }),
-    // Don't include any locale strings in the client JS bundle
-    { ...localesPlugin.vite({ locales: [] }), enforce: "pre" },
+    // TODO: Don't include any locale strings in the client JS bundle
+    {
+      ...localesPlugin.vite({ locales: [defaultLocale.culture] }),
+      enforce: "pre"
+    },
     tsconfigPaths()
   ]
 })
